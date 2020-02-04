e7861a3f9bc4b063cceb0ce6d201adfa20e92d90
eva <eva.domschot@student.nmt.edu>
2020-02-03 19:59:10 -0700

    added optarg options for command line

---
+++ build/CMakeFiles/compiler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler.dir/Scanner.cpp.o"
+  "CMakeFiles/compiler.dir/Parser.cpp.o"
+  "libcompiler.pdb"
+  "libcompiler.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
