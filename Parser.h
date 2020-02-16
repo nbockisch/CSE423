d1f84d5984bc8e776443e02aca50e6ccc3a16e96
Bicknell125 <39744106+Bicknell125@users.noreply.github.com>
2020-02-16 00:33:11 -0700

    Changes to .h to reflect activities in .cpp

--- Parser.h
+++ Parser.h
@@ -14,18 +14,22 @@
 enum prod_type { nonterminal, charclass, literal, keyw };
 
 struct ParsedToken {
-        std::string name;
-        prod_type tp;
-        bool trep; //for if we want to store whether or not this op needs to be in the parse tree
-        int priority;
-        int children;
+	std::string name;
+	prod_type tp;
+	bool trep; //for if we want to store whether or not this op needs to be in the parse tree
+	int priority;
+	int children;
 };
 
 
 struct Production {
-        prod_type type;
-        std::string name;
-        int level; // Level is currently the number of the production found as read in from file..
+	prod_type type;
+	std::string name;
+	int level; // Level is currently the number of the production found as read in from file..
+
+	bool operator==(const Production& x) {
+		return x.name == name && x.type == type;
+	}
 };
 
 typedef std::pair<std::string, std::vector<std::vector<Production>>> Rule;
@@ -39,8 +43,8 @@ public:
         int verifyGrammar();
         void printRules();
         
-        std::vector<ParsedToken> releasePV();
-        int verify(std::vector<std::string> tokens);
+        std::vector<ParsedToken> releasePV() {return PV;}
+		int search(std::vector<std::string> tokens);
 
 private:
         std::string fname;
@@ -49,7 +53,12 @@ private:
         
         
         std::vector<ParsedToken> PV;
-        int search(std::string token);
+		std::vector<std::string> search_stack;
+
+		int bytype(std::vector<Rule> bfs, std::vector<int> ruleindex, int depth);
+		int recalculate_priority(std::string rule);
+		Production match(std::string left);
+		int verify();
         //std::string term_match(std::string);
 
 };
