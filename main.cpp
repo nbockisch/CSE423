bbf1a4c849dd853aa66f6f9fbc54624f77243f19
eva <eva.domschot@student.nmt.edu>
2020-02-17 08:20:28 -0700

    small changes

--- main.cpp
+++ main.cpp
@@ -10,17 +10,20 @@
 #include <vector>
 #include "Scanner.h"
 #include <unistd.h>
+#include "AST.h"
 
 
 int main(int argc, char **argv) {
 	int opt;
 	char *file;
-
+	int tree = 0;
 	/*break apart command line arguments*/
-	while((opt = getopt(argc, argv, ":if:lrx")) != -1) {  
+	while((opt = getopt(argc, argv, ":af:lrx")) != -1) {  
 		switch(opt)  { 
 			/*boiler plate: change letters to whatever you want as needed*/ 
-			case 'i':  
+			case 'a':  
+				tree = 1;
+				break;
 			case 'l':  
 			case 'r':  
 				printf("option: %c\n", opt);  
@@ -45,6 +48,16 @@ int main(int argc, char **argv) {
         Scanner *scanner = new Scanner(file);
 
 	std::vector<token_t> tokens = scanner->tokenize(scanner->readFile(size));
+	
+	AST *ast = new AST();
+	char const *order[3] = {"main", "return", "1"};
+       	ast->insert(order);
+	if (tree == 1) {
+		printf("Post order traversal of AST:\n");
+		ast->postorder();
+	}
+        delete ast;
  
         delete scanner;
+
 }
